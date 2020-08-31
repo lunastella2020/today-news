fba1d1706406c1c4d292e852e711e19ce7760b5f
lunastella2020 <seychelle86@gmail.com>
2020-08-31 21:12:05 +0900

    Updates

---
+++ precache-manifest.34485b724c7588e3be20708fb0dd531a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c71f3c590c767c587c8e741ffb421a",
+    "url": "/today-news/index.html"
+  },
+  {
+    "revision": "f178f5ae6af9157db0c6",
+    "url": "/today-news/static/css/main.72c93dbb.chunk.css"
+  },
+  {
+    "revision": "a06f9e569046521e7c4a",
+    "url": "/today-news/static/js/2.5ed4bcb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/today-news/static/js/2.5ed4bcb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f178f5ae6af9157db0c6",
+    "url": "/today-news/static/js/main.0844b542.chunk.js"
+  },
+  {
+    "revision": "2458702f60bc7ce3eadf",
+    "url": "/today-news/static/js/runtime-main.f333d071.js"
+  }
+]);
